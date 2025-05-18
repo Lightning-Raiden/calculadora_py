4ddb57b2658cb43c387e9ffd5bc23afc9d7cc0b0
Eric Filgueiras <ericfilgueiras.contato@gmail.com>
2025-05-18 10:03:30 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -9,27 +9,36 @@ def check_int(a):
 
     return a
 
-def soma(a, b):
-    return a + b
-
-def sub(a, b):
-    return a - b
-
-def mult(a, b):
-    return a * b
-
-def div(a, b):
-
-    while b == 0:
-        b = input("Digite um número diferente de zero: ")
-        b = check_int(b)
-        print("")
+def calculadora(valor1, valor2, operador):
     
-    return a / b
+    if operador == "+":
+        result = (valor1 + valor2)
+        return print(f"A soma dos números é igual a: {result}")
+    
+    elif operador == "-":
+        result = (valor1 - valor2)
+        return print(f"A subtração dos números é igual a: {result}")
+    
+    elif operador == "*":
+        result = (valor1 * valor2)
+        return print(f"A multiplicação dos números é igual a: {result}")
+    
+    elif operador == "/":
+
+        while valor2 == 0:
+            valor2 = input("Digite um número diferente de zero: ")
+            valor2 = check_int(valor2)
+            print("")
+    
+        result = (valor1 / valor2)
+        return print(f"A divisão dos números é igual a: {result}")
+    
+    else:
+        print("Algo deu errado! Por favor, tente novamente.")
+
 
 num1 = input("Digite o primeiro número: ")
 num1 = check_int(num1)
-
 
 num2 = input("Digite o segundo número: ")
 num2 = check_int(num2)
@@ -42,19 +51,18 @@ print("Multiplicação (*)")
 print("Divisão (/)")
 
 operacao = input("Digite o símbolo da operação que deseja fazer: ")
+
+while True:
+    try:
+        if operacao == "+" or operacao == "-" or operacao == "*" or operacao == "/":    
+            break
+
+        else:
+            raise ValueError
+        
+    except ValueError:
+        operacao = input("Operação inválida! Digite apenas um dos símbolos especificados acima: ")
+
 print("")
 
-if operacao == "+":
-    print(f"A soma dos números é igual a: {soma(num1, num2)}")
-
-elif operacao == "-":
-    print(f"A subtração dos números é igual a: {sub(num1, num2)}")
-
-elif operacao == "*":
-    print(f"A multiplicação dos números é igual a: {mult(num1, num2)}")
-
-elif operacao == "/":
-    print(f"A divisão dos números é igual a: {div(num1, num2)}")
-
-else:
-    print("Operação inválida! Digite apenas um dos símbolos especificados acima!")
+calculadora(num1, num2, operacao)
